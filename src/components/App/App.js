0fd9e706ec0145496e48a4e4e2b73face6aaeafb
phentality <pheonix09@yandex.ru>
2024-05-01 14:38:20 +0300

    Boost

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
 import Main from '../Main/Main';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
@@ -9,9 +8,7 @@ function App() {
   return (
     <div>
       <Header />
-      <Routes>
-        <Route path="/" element={<Main />} />
-      </Routes>
+      <Main />
       <Footer />
     </div>
   );
